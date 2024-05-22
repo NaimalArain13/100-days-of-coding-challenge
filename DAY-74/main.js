75a90eda1cb8984176a8a7d3ef910a854cc4eb05
Naimal Salahuddin <naimalarain13@gmail.com>
2024-05-22 13:56:30 +0500

    tasks added

---
+++ DAY-74/main.js
@@ -0,0 +1,20 @@
+"use strict";
+// DEBUGGING
+let age = 22;
+if (age > 20) {
+    console.log("pass");
+}
+else if (age < 20) {
+    console.log("return");
+}
+else {
+    console.log("reboot");
+}
+let number = 23;
+if (number % 2 == 0) {
+    console.log("Even", number);
+}
+else {
+    console.log("Odd", number);
+}
+//# sourceMappingURL=main.js.map
---
+++ DAY-74/main.ts
@@ -0,0 +1,17 @@
+// DEBUGGING
+
+let age: number = 22;
+if (age > 20) {
+  console.log("pass");
+} else if (age < 20) {
+  console.log("return");
+} else {
+  console.log("reboot");
+}
+
+let number = 23;
+if(number % 2 == 0){
+    console.log("Even" , number);
+}else{
+    console.log("Odd" , number);  
+}
